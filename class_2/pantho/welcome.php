cacb6a9b9efab79dc4e3e74d75ab89400501bba0
PANTHO\Pantho <probal.sutradhar@northsouth.edu>
2018-04-28 13:36:32 +0600

    my second code
    done

--- class_2/pantho/welcome.php
+++ class_2/pantho/welcome.php
@@ -5,17 +5,8 @@
 Your email address is: <?php echo $_GET["password"]; ?> -->
 
 <?php 
-	if($_GET["pantho"]==input)
-	{
-		header("Location: http://www.facebook.com");
-		die();
-	}
-
-	else
-	{
-		header("Location: http://www.google.com");
-		die();
-	}
+	<?php 
+	echo $_GET["name"];
 ?>
 
 </body>
