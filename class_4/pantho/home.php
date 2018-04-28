80d363acc349e833141bf1587683ef37808216d0
PANTHO\Pantho <probal.sutradhar@northsouth.edu>
2018-04-28 14:42:19 +0600

    class 4
    task

---
+++ class_4/pantho/home.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>CODE</title>
+</head>
+<body>        
+        <div class="nav1 ">
+            
+                <div class="nav col-lg-12 col-md-12 col-sm-12 col-xs-12 text-center center-block">
+                    <ul class="list-unstyled list-inline ">
+                        <li><a class="a" href="#">HOME</a></li>
+                        <li><a class="a" href="#">ABOUT ME</a></li>
+                        <li><a class="a" href="#">CONTACT</a></li>
+                    </ul>
+                </div>
+                
+            </div>
+           
+        </div>
+</body>
+</html>
