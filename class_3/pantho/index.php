5b88eb2c5584ac6b0678bd535da119f35bed518d
PANTHO\Pantho <probal.sutradhar@northsouth.edu>
2018-04-28 13:54:24 +0600

    index
    push

---
+++ class_3/pantho/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>CODE</title>
+</head>
+<body>
+  <form action="welcome.php" method="get">
+        <fieldset>
+          
+          <div class="form-group">
+              <input class="form-control" placeholder="User Name" name="pantho" type="text" autofocus>
+          </div>
+          <br>
+          <div class="form-group">
+              <input class="form-control" placeholder="User Name" name="pantho" type="text" autofocus>
+          </div>
+                                           
+            <input type="submit" name="Test">
+         </fieldset>
+    </form>
+
+</body>
+</html>
---
+++ class_3/pantho/welcome.php
@@ -0,0 +1,13 @@
+<html>
+<body>
+
+<!-- Welcome <?php echo $_GET["name"]; ?><br>
+Your email address is: <?php echo $_GET["password"]; ?> -->
+
+<?php 
+ 
+	echo $_GET["pantho"];
+?>
+
+</body>
+</html>
