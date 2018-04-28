76ddf972113139ffc0871e9d4183752548d2d83d
Parijat642 <33665873+Parijat642@users.noreply.github.com>
2018-04-28 13:01:07 +0600

    This is my first project

---
+++ class_1/parijat/welcome.php
@@ -0,0 +1,20 @@
+<html>
+<body>
+
+
+<?php 
+	if($_GET["parijat"]==input)
+	{
+		header("Location: http://www.facebook.com");
+		die();
+	}
+
+	else
+	{
+		header("Location: http://www.google.com");
+		die();
+	}
+?>
+
+</body>
+</html>
