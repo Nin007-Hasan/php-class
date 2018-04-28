369e331c1c5242cbdad6deb4c01d31bfd99b2831
PANTHO\Pantho <probal.sutradhar@northsouth.edu>
2018-04-28 13:55:13 +0600

    again
    push

--- class_3/pantho/index.php
+++ class_3/pantho/index.php
@@ -8,11 +8,11 @@
         <fieldset>
           
           <div class="form-group">
-              <input class="form-control" placeholder="User Name" name="pantho" type="text" autofocus>
+              <input class="form-control" placeholder="User Name" name="pan" type="text" autofocus>
           </div>
           <br>
           <div class="form-group">
-              <input class="form-control" placeholder="User Name" name="pantho" type="text" autofocus>
+              <input class="form-control" placeholder="User Name" name="tho" type="text" autofocus>
           </div>
                                            
             <input type="submit" name="Test">
