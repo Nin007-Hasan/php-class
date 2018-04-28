03c999a4f894710ce95e1868f7b2b8e907837770
deboprio <rasmani92@gmail.com>
2018-04-28 15:02:51 +0600

    header footer using a extra file  where  iwe used include  tag

---
+++ class_4/mani/aboutus.php
@@ -0,0 +1,26 @@
+
+
+<!DOCTYPE html>
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<? php include "header.php" ?>
+</head>
+<body>
+​
+<div class="topnav">
+  <a class="active" href="#index.php">Home</a>
+  
+  <a href="contact.php">Contact</a>
+  <a href="aboutus.php">About</a>
+</div>
+​
+<div style="padding-left:16px">
+  <h2>i have to fuck</h2>
+  <p>About us</p>
+</div>
+​​<?php include "footer.php" ?>
+</body>
+</html>
+​
+
---
+++ class_4/mani/index.php
@@ -0,0 +1,26 @@
+
+
+<!DOCTYPE html>
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<? include "header.php" ?>
+</head>
+<body>
+​
+<div class="topnav">
+  <a class="active" href="index.php">Home</a>
+  <a href="index.php">index</a>
+  <a href="contact.php">Contact</a>
+  <a href="aboutus.php">About</a>
+</div>
+​
+<div style="padding-left:16px">
+  <h2>hi i am debo</h2>
+  <p>index page</p>
+</div>
+<?php include "footer.php"?>
+</body>
+</html>
+​
+
