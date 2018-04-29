bca3b89f7c85a673a364066dbe9e4beb6559f94e
ashraf1410499 <abuashraf501@gmail.com>
2018-04-29 20:52:35 +0600

    sum of 2 boxes
    sums up 2 numbers and shows the output to another page

---
+++ class_3/ashraf/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>CODE</title>
+</head>
+<body>
+	<form action="welcome.php" method="get">
+        <fieldset>
+        	<div class="form-group">
+            	<input class="form-control" placeholder="User 1st number:" name="first" type="text" autofocus>
+            </div>
+                                           
+          
+            <div class="form-group">
+            	<input class="form-control" placeholder="User 2nd number:" name="second" type="text" autofocus>
+            </div>
+                                           
+            <input type="submit" name="Test">
+         </fieldset>
+    </form>
+
+</body>
+</html>
---
+++ class_3/ashraf/welcome.php
@@ -0,0 +1,17 @@
+<html>
+<body>
+
+<!-- Welcome <?php echo $_GET["name"]; ?><br>
+Your email address is: <?php echo $_GET["password"]; ?> -->
+
+<?php 
+$x=$_GET["first"];
+$y=$_GET["second"];
+$z=$x+$y;
+echo "sum:".$z;
+
+
+?>
+
+</body>
+</html>
