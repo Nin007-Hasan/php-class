7db73993bf3cab1e29a301091f6fe4987f62cf5f
Dhrubo <isrukhasan@gmail.com>
2018-05-01 15:05:19 +0600

    class 1
    rduyjbjnjb

---
+++ class_1/dhrubo/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>PHP CLASS</title>
+
+</head>
+
+<body>
+
+	 <form action="welcome.php" method="get">
+	
+	 <div class="forming">
+
+  First name:<br>
+  <input type="text" placeholder="first name" name="name"><br>
+
+  <input type="submit" value="Submit It">
+	
+	</div>
+	</form> 
+</body>
+</html>
---
+++ class_1/dhrubo/welcome.php
@@ -0,0 +1,12 @@
+<?php
+ if ($_GET["name"]==input) {
+ 	# code...
+ 	header("Location: https://www.google.com/");
+ 	die();
+ }
+
+ else
+ {
+ 	header("Location: index.php");
+ }
+?>
