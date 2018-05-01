f0e405e52f7ecb2b5b070b6c8444956114038fa7
Dhrubo <isrukhasan@gmail.com>
2018-05-01 15:23:18 +0600

    Done With Class 4
    Get Method

---
+++ class_4/dhrubo/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>PHP CLASS</title>
+
+</head>
+
+<body>
+
+	 <form action="welcome.php" method="get">
+	
+	 <div class="forming">
+
+  First number:<br>
+  <input type="text" placeholder="First Number" name="num1"><br>
+  Second number:<br>
+  <input type="text" placeholder="Second Number" name="num2"><br>
+
+  <input type="submit" value="add them">
+	
+	</div>
+	</form> 
+</body>
+</html>
---
+++ class_4/dhrubo/welcome.php
@@ -0,0 +1,8 @@
+<?php
+ $x=$_GET["num1"];
+ $y=$_GET["num2"];
+
+ $z=$x+$y;
+ echo "$z";
+
+?>
